d669e052803c6ef02bace46cb898245ab481ad7c
wisemendevp <dev@vqs.com.au>
2019-02-20 19:25:36 +0530

    Update CDVLocation.m

--- src/ios/CDVLocation.m
+++ src/ios/CDVLocation.m
@@ -141,12 +141,14 @@
         __highAccuracyEnabled = YES;
         // Set distance filter to 5 for a high accuracy. Setting it to "kCLDistanceFilterNone" could provide a
         // higher accuracy, but it's also just spamming the callback with useless reports which drain the battery.
-        self.locationManager.distanceFilter = 5;
+        //self.locationManager.distanceFilter = 5;
         // Set desired accuracy to Best.
+        self.locationManager.distanceFilter = kCLDistanceFilterNone;
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     } else {
         __highAccuracyEnabled = NO;
-        self.locationManager.distanceFilter = 10;
+        //self.locationManager.distanceFilter = 10;
+        self.locationManager.distanceFilter = kCLDistanceFilterNone; //NEW CODE
         self.locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
     }
 }
